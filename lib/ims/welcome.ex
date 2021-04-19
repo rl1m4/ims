2b9b08679070c6009565765c39aab0410791d5cc
rlima <rlima@atlassian.com>
2021-04-19 23:41:38 +1000

    add module welcome and function to receive params

---
+++ lib/ims/welcome.ex
@@ -0,0 +1,11 @@
+defmodule ims.welcome do
+  #  --data---->[Module.function]-data_modified-->[Module.function2]-->Result
+  # Receive name and age - if user has name x and age x, receives a special msg
+  # if user is age y -> receives a normal msg
+  # if user is age w -> returns an error
+  # Check for wrong user names
+
+  def welcome(params) do
+
+  end
+end
