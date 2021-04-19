c942b90211447cb627d6b277b20a23e83e90672c
rlima <rlima@atlassian.com>
2021-04-19 23:56:55 +1000

    Add function receiving name and params to test iex -S mix

--- lib/ims/welcomer.ex
+++ lib/ims/welcomer.ex
@@ -1,4 +1,4 @@
-defmodule ims.welcome do
+defmodule Ims.Welcome do
   #  --data---->[Module.function]-data_modified-->[Module.function2]-->Result
   # Receive name and age - if user has name x and age x, receives a special msg
   # if user is age y -> receives a normal msg
@@ -6,6 +6,10 @@ defmodule ims.welcome do
   # Check for wrong user names
 
   def welcome(params) do
+    name = params["name"]
+    age = params["age"]
 
+    IO.inspect(name)
+    IO.inspect(age)
   end
 end
