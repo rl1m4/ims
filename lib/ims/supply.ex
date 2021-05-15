f05cf140446bd1982dbd6c529535dde73f79d96d
rlima <rlima@atlassian.com>
2021-05-16 01:11:31 +1000

    Add supply db schema

--- lib/ims/supply.ex
+++ lib/ims/supply.ex
@@ -1,38 +1,36 @@
-defmodule Ims.Restaurant do
+defmodule Ims.Supply do
   use Ecto.Schema
   # https://hexdocs.pm/ecto/Ecto.Changeset.html
   # ecto documentation, validation, schema, etc
   import Ecto.Changeset
 
-  alias Ims.Supply
+  alias Ims.Restaurant
 
   # module variable @sample
   @primary_key {:id, :binary_id, autogenerate: true}
 
-  @required_params [:name, :email]
+  @required_params [:description, :expiration_date, :responsible, :restaurant_id]
 
   # render  structs to json - ie. Restaurants struct
   @derive {Jason.Encoder, only: @required_params ++ [:id]}
 
-  schema "restaurants" do
-    field :email, :string
-    field :name, :string
+  schema "supplies" do
+    field :description, :string
+    field :expiration_date, :date
+    field :responsible, :string
 
-    has_many :supplies, Supply
+    belongs_to :restaurant, Restaurant
 
     timestamps()
   end
 
   def changeset(params) do
-    # replace to Ims.Restaurant
+    # replace to Ims.Supply
     %__MODULE__{}
     # atom list
     |> cast(params, @required_params)
-    # validation
     |> validate_required(@required_params)
-    |> validate_length(:name, min: 2)
-    # regex email
-    |> validate_format(:email, ~r/@/)
-    |> unique_constraint([:email])
+    |> validate_length(:description, min: 3)
+    |> validate_length(:responsible, min: 3)
   end
 end
